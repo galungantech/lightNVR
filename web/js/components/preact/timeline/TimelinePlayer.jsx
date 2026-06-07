643e62af43f5d89d205cb86dd33590e5aeb31798
Rivandraa <139041404+rivandraa@users.noreply.github.com>
2026-06-07 14:06:51 +0800

    Update TimelinePlayer.jsx

--- web/js/components/preact/timeline/TimelinePlayer.jsx
+++ web/js/components/preact/timeline/TimelinePlayer.jsx
@@ -472,11 +472,11 @@ export function TimelinePlayer({ videoElementRef = null, autoFullscreen = false 
     }
 
     // === POSISI KODE BARU YANG HARUS KAMU MASUKKAN ===
-    if (video.playbackRate > 1) {
-      updateTimeDisplay(currentTime, segment);
-      lastTimeUpdateRef.current = currentTime;
-      return; // Berhenti di sini, jangan update state global timeline
-    }
+//    if (video.playbackRate > 1) {
+//      updateTimeDisplay(currentTime, segment);
+//      lastTimeUpdateRef.current = currentTime;
+//      return; // Berhenti di sini, jangan update state global timeline
+//    }
     
     // Update timeline state with the current time
     timelineState.setState({
